c201f3d5e32cda6e84aae192d7bd405914966dcd
AdnanAshraf <adnanashraf.9011@gmail.com>
2023-08-23 14:13:22 +0500

    add  middleware

--- ChatGPT/middlewares.py
+++ ChatGPT/middlewares.py
@@ -1,13 +1,15 @@
-# from django.http import HttpResponseForbidden
-#
-#
-# class AdminHostMiddleware:
-#     def __init__(self, get_response):
-#         self.get_response = get_response
-#
-#     def __call__(self, request):
-#         # Check if the HTTP_HOST matches the allowed host
-#         if request.META.get('HTTP_HOST') == '34.217.76.153' and 'admin' in request.path:
-#             return self.get_response(request)
-#         else:
-#             return HttpResponseForbidden("Access Denied")
+from django.http import HttpResponseForbidden
+
+
+class AdminHostMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Check if the HTTP_HOST matches the allowed host
+        if request.META.get('HTTP_HOST') == '34.217.76.153' and 'admin' in request.path:
+            return self.get_response(request)
+        elif request.META.get('HTTP_HOST') == '34.217.76.153' and 'admin' not in request.path:
+            return HttpResponseForbidden('<h1>Forbidden</h1>')
+        else:
+            return self.get_response(request)
